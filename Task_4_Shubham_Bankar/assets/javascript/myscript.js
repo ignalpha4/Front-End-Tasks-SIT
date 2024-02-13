baab2970b099c2bcb1c7d4a72e1cd76cb709d470
Shubham Bankar <sit461@sit.local>
2024-02-13 17:25:57 +0530

    completed sidebar

--- Task_4_Shubham_Bankar/assets/javascript/myscript.js
+++ Task_4_Shubham_Bankar/assets/javascript/myscript.js
@@ -1,13 +1,12 @@
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "345px";
+  document.getElementById("header").style.marginLeft = "345px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("header").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
-  }
-
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "345px";
-    document.getElementById("header").style.marginLeft = "345px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
