151abbc29eea0bb0fee1366a79355c4c754a8eb0
ignalpha4 <shubhambankar444@gmail.com>
2024-02-28 11:26:54 +0530

    task 3 completed with validation and table features working

--- Javascript_Tasks/JS_Task_3_Shubham_Bankar/assets/javascript/script.js
+++ Javascript_Tasks/JS_Task_3_Shubham_Bankar/assets/javascript/script.js
@@ -1,7 +1,10 @@
+//to store the details of the student
+
 let student_data = []; 
 
 //-----------------row addition-------------------------------
 function add_row() {
+
     let new_row = document.createElement('div');
     new_row.classList.add('row', 'education_row');
 
@@ -84,6 +87,150 @@ function submit_data(event) {
     event.preventDefault();
 
     let edit_index = document.getElementById('edit_index').value;
+
+    //----------------All validation checks--------------------
+        //fname validation
+
+        if(validate_fname()==false){
+            return false;
+        }
+    
+        if(validate_fname()==true){
+            document.querySelector("#fname").classList.remove("is-invalid");
+            document.querySelector("#fname_v").innerHTML=`
+            <p></p>`
+        }
+    
+        //lname validation
+    
+        if(validate_lname()==false){
+            return false;
+        }
+    
+        if(validate_lname()==true){
+            document.querySelector("#lname").classList.remove("is-invalid");
+            document.querySelector("#lname_v").innerHTML=`
+            <p></p>`
+        }
+    
+        //email
+    
+        if(validate_mail()==false){
+            return false;
+        }
+    
+        if(validate_mail()==true){
+            document.querySelector("#email").classList.remove("is-invalid");
+            document.querySelector("#email_v").innerHTML=`
+            <p></p>`
+        }
+    
+        // dob validation
+    
+        if(dob_validation()==false)
+        {
+            return false;
+        }
+       
+        if(dob_validation()==true){
+            document.querySelector("#dob").classList.remove("is-invalid");
+            document.querySelector("#dob_validate").innerHTML=`
+            <p></p>`
+        }
+    
+    
+        //-----------------------------education validation-------------------------
+    
+        //degree +college 2 fields
+    
+        let rows = document.querySelectorAll('.education_row');
+        let degree_clg_valid = true;
+    
+        rows.forEach(row => {
+            if (!validate_degree(row) || !validate_college(row)) {
+                degree_clg_valid = false;
+            }
+            if(validate_degree(row)){
+                row.querySelector(".degree").classList.remove("is-invalid");
+                row.querySelector(".degree_v").innerHTML = "";
+            }
+            if(validate_college(row)){
+                       
+            row.querySelector(".college").classList.remove("is-invalid");
+            row.querySelector(".college_v").innerHTML = "";
+            }
+        });
+    
+        
+        if (!degree_clg_valid) {
+            return false;
+        }
+    
+    
+    
+    
+        //passout
+    
+        rows = document.querySelectorAll('.education_row');
+        let passout_valid = true;
+    
+        rows.forEach(row => {
+            if (!validate_passout(row)) {
+                passout_valid = false;
+            }
+    
+            if(validate_passout(row)){
+                      
+            row.querySelector(".passout_date").classList.remove("is-invalid");
+            row.querySelector(".passout_v").innerHTML = "";
+            }
+        });
+    
+        
+        if (!passout_valid) {
+            return false;
+        }
+    
+        //percentage
+    
+        rows=document.querySelectorAll(".education_row");
+        let valid_per=true;
+    
+        rows.forEach(row=>{
+            if(!validate_percentage(row)){
+                valid_per=false;
+            }
+    
+            if(validate_percentage(row)){
+                row.querySelector(".percentage").classList.remove("is-invalid");
+                row.querySelector(".p_v").innerHTML = "";
+            }
+        })
+    
+        if(!valid_per){
+            return false;
+        }
+    
+        //backlogs
+    
+        rows=document.querySelectorAll(".education_row");
+        let valid_backlogs=true;
+    
+        rows.forEach(row=>{
+            if(!validate_backlog(row)){
+                valid_backlogs=false;
+            }
+    
+            if(validate_backlog(row)){
+                row.querySelector(".backlog").classList.remove("is-invalid");
+                row.querySelector(".backlog_v").innerHTML = "";
+            }
+        })
+    
+        if(!valid_backlogs){
+            return false;
+        }
+
     
     // if index is set to -1 then add a new row/new user
     if (edit_index === '-1') {
@@ -114,6 +261,7 @@ function submit_data(event) {
     } else {
         // else we update that same row
         let student = student_data[edit_index];
+
         student.first_name = document.getElementById('fname').value;
         student.last_name = document.getElementById('lname').value;
         student.dob = document.getElementById('dob').value;
@@ -122,6 +270,7 @@ function submit_data(event) {
         student.graduationYear = document.getElementById('g_year').value;
 
         let education_rows = document.querySelectorAll('.education_row');
+        //clearing the values first
         student.education = [];
         education_rows.forEach(row => {
             let education_details = {
@@ -135,8 +284,8 @@ function submit_data(event) {
             student.education.push(education_details);
         });
     }
-
-    // to reupdate the tables with new data
+  
+    // to re-update the tables with new data
     update_tables();
 
     document.querySelector('form').reset();  //to clear the form inputs
@@ -145,6 +294,7 @@ function submit_data(event) {
 
     
 }
+
 //json datatables
 $(document).ready(function() {
     $('.t1').DataTable();
@@ -286,4 +436,166 @@ function close_modal() {
     modal.setAttribute('aria-modal', 'false');
     modal.setAttribute('aria-hidden', 'true');
 
+}
+
+
+//-------------whole validation code-----------------
+
+//fname validation
+function validate_fname(){
+    let fname=document.getElementById("fname").value;
+
+    let reg = /^[a-zA-Z]+$/;
+
+    if(!reg.test(fname)){
+        
+        document.querySelector("#fname").classList.add("is-invalid");
+        document.querySelector("#fname_v").innerHTML=`
+        <p style="color:red">Name should contain only alphabets</p>
+        `
+        return false;
+    }
+
+    return true;
+}
+
+//lname validation
+function validate_lname(){
+    let lname=document.getElementById("lname").value;
+
+    let reg = /^[a-zA-Z]+$/;
+
+    if(!reg.test(lname)){
+        
+        document.querySelector("#lname").classList.add("is-invalid");
+        document.querySelector("#lname_v").innerHTML=`
+        <p style="color:red">Name should contain only alphabets</p>
+        `
+        return false;
+    }
+
+    return true;
+}
+//dob validation
+function dob_validation(){
+
+    let date=document.querySelector("#dob").value;
+
+    const array = date.split("-");
+
+    if(array[0] > 2006){
+
+        document.querySelector("#dob").classList.add("is-invalid");
+        document.querySelector("#dob_validate").innerHTML=
+
+        `<p style="color:red">Age cannot be less than 18</p>
+        `
+
+        return false;
+    }
+
+ return true;
+}
+
+function validate_mail(){
+    let mail=document.querySelector("#email").value;
+
+    let reg=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ ;
+
+    if(!reg.test(mail)){
+        document.querySelector("#email").classList.add("is-invalid");
+        document.querySelector("#email_v").innerHTML=`
+        <p style="color:red">Please enter a valid email address</p>`
+        return false;
+    }
+
+    return true;
+}
+
+
+//--------------------education validation-----------------------------
+
+function validate_degree(row) {
+    let degree = row.querySelector(".degree").value;
+
+    let reg = /^[a-zA-Z\s]+$/;
+
+    if (!reg.test(degree)) {
+        row.querySelector(".degree").classList.add("is-invalid");
+        row.querySelector(".degree_v").innerHTML = `
+            <p style="color:red">Enter the correct Board/Degree</p>
+        `;
+        return false;
+    } 
+    return true;
+}
+
+
+function validate_college(row) {
+    let college = row.querySelector(".college").value;
+    let reg = /^[a-zA-Z\s]+$/;
+
+    if (!reg.test(college)) {
+        row.querySelector(".college").classList.add("is-invalid");
+        row.querySelector(".college_v").innerHTML = `
+            <p style="color:red">Enter the correct School/College Name</p>
+        `;
+        return false;
+    } 
+
+    return true;
+}
+
+
+
+function validate_passout(row) {
+    let start_date = row.querySelector(".start_date").value;
+    let passout_date = row.querySelector(".passout_date").value;
+
+    let start = start_date.split("-");
+    let passout = passout_date.split("-");
+
+    if (start[0] >= passout[0]) {
+        row.querySelector(".passout_date").classList.add("is-invalid");
+        row.querySelector(".passout_v").innerHTML = `
+            <p style="color:red">Passout year cannot be less or equal to start date</p>
+        `;
+        return false;
+    }
+    return true;
+}
+
+
+//validate Percentage
+
+function validate_percentage(row){
+
+    let per=row.querySelector(".percentage").value;
+
+    if(per<0 || per>100){
+        row.querySelector(".percentage").classList.add("is-invalid");
+        row.querySelector(".p_v").innerHTML = `
+            <p style="color:red">Enter Percentage between 0 and 100</p>
+        `;
+        return false;
+    }
+
+    return true;
+}
+
+// validate backlogs
+
+function validate_backlog(row){
+
+    let logs=row.querySelector(".backlog").value;
+
+    if((logs<0) || (logs>15)){
+        row.querySelector(".backlog").classList.add("is-invalid");
+        row.querySelector(".backlog_v").innerHTML = `
+            <p style="color:red">Backlogs must be between 0-15</p>
+        `;
+        return false;
+    }
+
+    return true;
 }
